352111e0761cd618a04f05ca35c2939579cbf7b2
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-01-26 11:57:51 +0530

    added prisma accelerate

--- src/lib/database/prisma.js
+++ src/lib/database/prisma.js
@@ -1,9 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 let prisma
 
 if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient()
+	prisma = new PrismaClient().$extends(withAccelerate())
 } else {
 	if (!global.prisma) {
 		global.prisma = new PrismaClient()
