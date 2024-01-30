3e8f67884ef0d570e64d05a3029414bfbc750e5a
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-01-31 04:41:43 +0530

    changed link variant on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
+import { buttonVariants } from '@/components/ui/button'
+
 export default function Home() {
 	return (
 		<main className="h-screen flex justify-center items-center">
 			<div className="text-center">
-				<h1>Welcome to the Home Page</h1>
-				<Link href={'/chat/nextjs'} className="text-blue-400">
+				<h1 className="my-2">Chat with the latest Next Js 14 documentation.</h1>
+				<Link href={'/chat/nextjs'} className={buttonVariants({ variant: 'outline' })}>
 					Chat with Nextjs
 				</Link>
 			</div>
