27ff26173e95f50bd470c9b276b54aa936481a98
Nikhil Sharma <niikkhilsharma@gmail.com>
2024-02-12 13:43:49 +0530

    added github user profile route in next config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ const nextConfig = {
 				hostname: 'picsum.photos',
 				pathname: '**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+				pathname: '**',
+			},
 		],
 	},
 }
